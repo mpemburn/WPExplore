980980df1ff6f6760845ae0dbe94e9ba4871196b
mpemburn <mpemburn@clarku.edu>
2023-01-06 15:39:16 -0500

    Add rough Dusk test.

---
+++ tests/Browser/PageWasFoundTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Browser;
+
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class PageWasFoundTest extends DuskTestCase
+{
+    public function test_page_was_found(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('https://wordpress.test.clarku.edu/polypeet/the-polyporales-post/www.mycologia.org/content/early/2012/09/06/12-088.abstract')
+                ->assertNotSee('Not Found');
+        });
+    }
+}
