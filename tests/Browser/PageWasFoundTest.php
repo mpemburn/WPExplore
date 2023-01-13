9b70222963d9e40010926b3ad55e48998ce75cd8
Mark Pemburn <markpemburn@trak.io>
2023-01-13 10:22:05 -0500

    Add Dusk tests.

--- tests/Browser/PageWasFoundTest.php
+++ tests/Browser/PageWasFoundTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser;
 
+use App\Models\WordPressTestBrokenPage;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
@@ -11,7 +12,29 @@ class PageWasFoundTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('https://wordpress.test.clarku.edu/polypeet/the-polyporales-post/www.mycologia.org/content/early/2012/09/06/12-088.abstract')
-                ->assertNotSee('Not Found');
+                ->assertSee('Not Found');
         });
+    }
+
+    public function test_page_fatal_error(): void
+    {
+        WordPressTestBrokenPage::query()
+            ->where('error', 'LIKE', '500 %')
+            ->each(function ($page){
+                if (stripos($page->page_url, '.pdf') !== false) {
+                    return;
+                }
+                if (stripos($page->page_url, '.jpg') !== false) {
+                    return;
+                }
+                if (stripos($page->page_url, '.png') !== false) {
+                    return;
+                }
+                var_dump($page->page_url);
+                $this->browse(function (Browser $browser) use ($page) {
+                    $browser->visit($page->page_url)
+                        ->assertDontSee('critical error');
+                });
+         });
     }
 }
