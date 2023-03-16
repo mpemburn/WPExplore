db956a1f7f34ba57c05e1e5b2f91f1ffc0ba4727
Mark Pemburn <markpemburn@trak.io>
2023-03-16 08:00:08 -0400

    Add fatal error tests

--- tests/Browser/NoFatalErrorTest.php
+++ tests/Browser/NoFatalErrorTest.php
@@ -8,19 +8,39 @@ use Tests\DuskTestCase;
 
 class NoFatalErrorTest extends DuskTestCase
 {
+    const ERRORS_FOUND = [
+        'https://www.clarku.edu/critical-data-studies/collaborative-glossary/data-colonialism.php',
+        'https://www.clarku.edu/report/authors/report.authors.php',
+        'https://www.clarku.edu/sharer/sharer.php',
+        'https://www.clarku.edu/sysadmin/login.php',
+        'http://www.clarku.edu/users/login.php',
+        'https://www.clarku.edu/course/view.php',
+        'http://www.clarku.edu/web/index.php',
+        'http://www.clarku.edu/s/t-facts.php',
+        //'https://www.clarku.edu/clark-together/video-categories/',
+        'https://www.clarku.edu/medicine/about/student-resources/financial-aid/international-student-info.php',
+        'http://www.clarku.edu/Admissions/mdadmissionsrequirements.php',
+        'http://www.clarku.edu/com/admissions/international.php',
+        //'https://www.clarku.edu/admissions/graduate-admissions/nyu-grad-admissions-blog/international-students/navigating-the-job-search-as-an-international-student.html',
+        //'https://www.clarku.edu/contact-us/',
+        //'https://www.clarku.edu/career-experience/internships-volunteer-work-and-employment/',
+        //'https://www.clarku.edu/life-at-clark/meet-our-student-life-team/',
+        'https://www.clarku.edu/undergraduate-admissions/apply/international-students/',
+    ];
+
     public function test_page_fatal_error(): void
     {
-        DevBrokenPage::query()->where('error', 'LIKE', '%500%')
+        collect(self::ERRORS_FOUND)
             ->each(function ($page) {
-                if (preg_match('/.*\.(?:jpe?g|png|gif|pdf)(?:\?\S+)?$/', $page->page_url)) {
+                if (preg_match('/.*\.(?:jpe?g|png|gif|pdf)(?:\?\S+)?$/', $page)) {
                     return;
                 }
-                var_dump($page->page_url);
+                echo 'Testing: ' . $page . PHP_EOL;
                 $this->browse(function (Browser $browser) use ($page) {
-                    $browser->visit($page->page_url)
-                        ->assertDontSee('critical error');
-                    $browser->visit($page->page_url)
-                        ->assertDontSee('Non-existent changeset UUID.');
+                    $browser->visit($page)
+                        ->assertSee('Internal Server Error');
+//                    $browser->visit($page)
+//                        ->assertDontSee('Non-existent changeset UUID.');
                 });
             });
     }
